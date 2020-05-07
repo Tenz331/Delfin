f7cd52e0c07b3ec27b9ca9c5f3dbb44463f4924d
Mathias <mathiastouboel@gmail.com>
2020-05-07 11:50:09 +0200

    clean up 2

--- src/main/java/Datamapper/MemberRead.java
+++ src/main/java/Datamapper/MemberRead.java
@@ -1,33 +1,6 @@
 package Datamapper;
-
-
-
+import Util.DBConnect;
 import java.sql.*;
-import java.util.ArrayList;
-
 public class MemberRead {
-    ArrayList<String> tempPizzas = new ArrayList<String>();
-    private static final String USERNAME = "fullroot";
-    private static final String PASSWORD = "fullroot";
-    private static final String CONN_STR = "jdbc:mysql://cphb-gruppe1.c4mqzn3xlkdy.us-east-2.rds.amazonaws.com/";
-    public ArrayList<String> getQueryJDBC() {
-        //'Connection', 'Statement' and 'ResultSet' are AUTO-CLOSABLE when with TRY-WITH-RESOURCES BLOCK (...)
-        try (
-                Connection conn = DriverManager.getConnection(CONN_STR, USERNAME, PASSWORD);
-                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                ResultSet rs = ((Statement) stmt).executeQuery("SELECT * FROM MariosPizza.pizzasMenu")
-        ) {
-            while (rs.next()) {
-
-                StringBuffer buffer = new StringBuffer();
-                buffer.append("Pizza: " + rs.getInt("pizza_id") + " : " + rs.getString("pizza_name") + ", Price: " + rs.getDouble("pizza_price"));
-                String temp = new String(("pizza_name"));
-                tempPizzas.add(temp);
-            }
-        } catch (SQLException e) {
-            //Different error messages
-            System.out.println(e);
-        }
-        return tempPizzas;
-    }
+    Connection connection = DBConnect.getInstance().getConnection();
 }
--- src/main/java/Datamapper/MemberWrite.java
+++ src/main/java/Datamapper/MemberWrite.java
@@ -1,14 +1,8 @@
 package Datamapper;
 
+import Util.DBConnect;
+
 import java.sql.*;
-import java.util.ArrayList;
-
 public class MemberWrite {
-    int tempUID;
-    int tempPizzaCounter = 0;
-    private static final String USERNAME = "fullroot";
-    private static final String PASSWORD = "fullroot";
-    private static final String CONN_STR = "jdbc:mysql://cphb-gruppe1.c4mqzn3xlkdy.us-east-2.rds.amazonaws.com/";
-    //"SELECT * FROM MariosPizza.Ordre WHERE pizza_pizza_ordreNR ="+UID)
-
+    Connection connection = DBConnect.getInstance().getConnection();
 } //gør at brugeren kan modificere hans pizza I menukortet hvis den fx skal havde en ny pris eller navn
