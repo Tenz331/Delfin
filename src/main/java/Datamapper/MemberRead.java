643843898a8f5319d5d611277734750f65450f21
janussr <Janusstivang@gmail.com>
2020-05-08 14:06:59 +0200

    Janus set members den nyeste

--- src/main/java/Datamapper/MemberRead.java
+++ src/main/java/Datamapper/MemberRead.java
@@ -4,6 +4,9 @@ import java.sql.*;
 public class MemberRead {
     Connection connection = DBConnect.getInstance().getConnection();
 
+    public MemberRead(int pizza_id, String pizza_name, double pizza_price, int pizza_status) {
+    }
+
     public void getMember(){
 
     }
@@ -15,7 +18,6 @@ public class MemberRead {
     public void getResult(){
 
     }
-
 
 
 }
